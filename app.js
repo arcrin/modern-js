f3d0cc1faec7941881f62d78dd349d153b9c83a2
arcia <andy-wen@hotmail.com>
2019-02-26 19:02:05 -0800

    from hom

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const lastName = "Johnson";
 const age = 36;
 const str = "Hello there the name is Arcia";
 const tags = "web design, web development, programming";
+const val1 = "This is the home branch";
+
 
 let val;
 
@@ -44,5 +46,6 @@ val = tags.split(",")
 val = str.replace("Arcia", "Jack");
 
 val = str.includes("foo");
+
 
 console.log(val);
