8cbfb794046b225712e8688eb8b09c6d38f4b885
wbai <wbai@deltacontrols.com>
2019-02-26 16:33:52 -0800

    Init Commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const firstName = "William";
+const lastName = "Johnson";
+const age = 36;
+const str = "Hello there the name is Arcia";
+const tags = "web design, web development, programming";
+
+let val;
+
+val = firstName + lastName;
+
+val = firstName + " " + lastName;
+
+val = "Brad ";
+val += "Traversy";
+
+val = "Hello, the name is " + firstName + ' and I am ' + age;
+
+val = "That's awesome, I can't wait";
+
+val = firstName.length;
+
+val = firstName.concat(" ", lastName);
+
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[0];
+
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('l');
+
+val = firstName.charAt('2');
+
+val = firstName.charAt(firstName.length - 1);
+
+val = firstName.substring(0, 4);
+
+val = firstName.slice(-3);
+
+val = str.split(" ");
+
+val = tags.split(",")
+
+val = str.replace("Arcia", "Jack");
+
+val = str.includes("foo");
+
+console.log(val);
