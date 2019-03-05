1a7f73c0a448b19a2b83780ef5a384d2360fc957
wbai <wbai@deltacontrols.com>
2019-03-04 17:05:36 -0800

    frome home

---
+++ cp7/7_12/app.js
@@ -0,0 +1,31 @@
+document.getElementById('button1').addEventListener('click', getText);
+
+document.getElementById('button2').addEventListener('click', getJson);
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// Get local text file data
+function getText() {
+  fetch('text.txt')
+    .then(res => res.text())
+    .then(data => {
+      console.log(data);
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(err => console.log(err));
+}
+
+// Get local json data
+function getJson() {
+  fetch('posts.json')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById('output').innerHTNL = output;
+    })
+    .catch(err => console.log(err));
+}
