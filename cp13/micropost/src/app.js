503cf3ecefba9783043c67b65e62b98161bebfc1
wbai <wbai@deltacontrols.com>
2019-03-20 16:30:33 -0700

    from work, micropost added

--- cp13/micropost/src/app.js
+++ cp13/micropost/src/app.js
@@ -1,2 +1,68 @@
-const greeting = 'Hello World';
-console.log(greeting);
+import {http} from './http';
+import {ui} from './ui';
+
+document.addEventListener('DOMContentLoaded', getPosts);
+
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+document.querySelector('#post').addEventListener('click', deletePost);
+
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+
+function getPosts(){
+  http.get('http://localhost:3000/posts')
+  .then(data => ui.showPosts(data))
+  .catch(err => console.log(err));
+}
+
+function submitPost(){
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  http.post('http://localhost:3000/posts', data)
+  .then(data => {
+    ui.showAlert('Post added', 'alert-success');
+    ui.clearFields();
+    getPosts();
+  })
+  .catch(err => console.log(err));
+}
+
+function deletePost(e) {
+  if(e.target.parentElement.classList.contains('delete')){
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')){
+      http.delete(`http://localhost:3000/posts/${id}`)
+      .then(data => {
+        ui.showAlert('Post removed', 'alert alert-success');
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    }
+  }
+  e.preventDefault();
+}
+
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')){
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    ui.fillForm(data);
+  }
+
+  e.preventDefault();
+}
