4158924f7b5c212fb3574c2a374f1ce4a2ac7860
arcia <andy-wen@hotmail.com>
2019-03-20 21:45:54 -0700

    from home

--- cp13/micropost/src/app.js
+++ cp13/micropost/src/app.js
@@ -5,9 +5,11 @@ document.addEventListener('DOMContentLoaded', getPosts);
 
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
-document.querySelector('#post').addEventListener('click', deletePost);
+document.querySelector('#posts').addEventListener('click', deletePost);
 
 document.querySelector('#posts').addEventListener('click', enableEdit);
+
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
 
 
 function getPosts(){
@@ -19,19 +21,33 @@ function getPosts(){
 function submitPost(){
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
   const data = {
     title,
     body
   }
-
-  http.post('http://localhost:3000/posts', data)
-  .then(data => {
-    ui.showAlert('Post added', 'alert-success');
-    ui.clearFields();
-    getPosts();
-  })
-  .catch(err => console.log(err));
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    if(id === '') {
+      http.post('http://localhost:3000/posts', data)
+      .then(data => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    } else {
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+        ui.showAlert('Post updated', 'alert alert-success');
+        ui.changeFormState('add');
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    }
+  }
 }
 
 function deletePost(e) {
@@ -62,6 +78,14 @@ function enableEdit(e) {
     }
 
     ui.fillForm(data);
+  }
+
+  e.preventDefault();
+}
+
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
   }
 
   e.preventDefault();
